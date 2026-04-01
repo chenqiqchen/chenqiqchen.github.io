a2b38ee66802eeea43c0573de993a98d595edc51
qi.chen <qi.chen@fargowealth.com.hk>
2026-04-01 15:01:09 +0800

    feat: 快速开始

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+    title: 'My Blog',
+    description: '技术博客',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '文章', link: '/guide/first-post' },
+        ],
+    },
+})
